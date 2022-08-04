f0f7dcbb61c202770df15f6d113b9ad996672b9b
Anmol <anmolgaur26@gmail.com>
2022-08-05 01:17:21 +0530

    created lodin endpoint in auth.js and major com

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const connecttomongo = require('./db');
-
+// connected to our database using code in db.js and moongoose
 connecttomongo();
 const app = express()
-const port = 3000
-
+const port = 5000
+// intead of keeping all routes here i created a seperate route  folder and used it as a package to  ensure the coed maintaninance 
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,82 @@
+// dont get afraid mainly alltehse fuction are from various node is libraries so anmol if u are coming after a long time u have just to go through the documentation of the dependencies
+
 const express = require('express');
 const { body, validationResult } = require('express-validator')
 const router = express.Router();
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
-router.post('/', [
+const JWT_SECRET = 'dattebayo';
+
+// Create a User using: POST "/api/auth/createuser and validating it useing express -validator
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err)
-     res.json({error: 'Please enter a unique value for email', message: err.message})})
+    // creating user and checking for errors
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const secpass = await bcrypt.hash(req.body.password, salt)
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secpass,
+        })
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json(authtoken)
+    }
+    catch (error) {
+        console.error(error.message)
+        res.status(500).send("some error occured");
+    }
 })
 
+// if someone will hit my  at this endpoint then the async function below will run  here i am  matching the the enterd credential of the user with the enteed credential and if the credential  matches  giving them an auth token usin JWT
+
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        const pcompare = bcrypt.compare(password, user.password)
+        if (!pcompare) {
+            return res.status(400).json({ error: "Please try to login with correct credentials" });
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({ authtoken })
+    }
+    catch (error) {
+        console.error(error.message)
+        res.status(500).send("some error occured");
+    }
+})
 module.exports = router
