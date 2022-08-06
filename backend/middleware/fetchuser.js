605247c6acb30d78e99805ad34a4418f89951206
Anmol <anmolgaur26@gmail.com>
2022-08-06 15:26:00 +0530

    created notes route and completed auth route

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,21 +1,20 @@
-var jwt=require('jsonwebtoken')
+var jwt = require('jsonwebtoken')
 
-const JWT_SECRET ='dattebayo@7';
+const JWT_SECRET = 'dattebayo@7';
 
-const fetchuser=(req,res,next)=>{
-const token = req.header('auth-token');   
-if (!token) {
-    res.status(401).send({ error: "Please authenticate using a valid token" })
+const fetchuser = (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const person = jwt.verify(token, JWT_SECRET)
+        req.user = person.user;
+        next();
+    }
+    catch (error) {
+        res.status(401).send({ error: "please authenticate with a valid token 8" })
+    }
 }
-try 
-{
-const person=jwt.verify(token,JWT_SECRET)
-req.user = person.user;
-next();    
-} 
-catch (error) 
-{
-res.status(401).send({error:"please authenticate with a valid token 8"}) 
-}
-}
-module.exports=fetchuser
+module.exports = fetchuser
+
