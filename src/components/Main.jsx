0dd86c1064a61121776fe6140c77e800d53e0be7
Anmol <anmolgaur26@gmail.com>
2023-08-08 16:29:06 +0530

    form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,23 @@
-import React, {useRef, useState,useContext } from 'react'
+import React, {useState,useContext } from 'react'
 import Notes from './Notes'
 import Notecontext from '../context/Notes/Notecontext'
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 
 export default function Main() {
   const { transcript, resetTranscript } = useSpeechRecognition();
-  const [isListening, setIsListening] = useState(false);
-  const microphoneRef = useRef(null);
   const handleListing = () => {
-    setIsListening(true);
     SpeechRecognition.startListening({
       continuous: true,
     });
   };
-  const stopHandle = () => {
-    setIsListening(false);
-    SpeechRecognition.stopListening();
+  const stopHandle = (e) => {
+    e.preventDefault()
     note.description=transcript
+    SpeechRecognition.stopListening(); 
   };
-
+  const reset =(e)=>{
+   resetTranscript() 
+  }
 
   const context = useContext(Notecontext)
   const {Addnote}=context
@@ -70,16 +69,19 @@ export default function Main() {
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
                required minLength={5}/>
                <button
-            type="submit"
             className="text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium m-2 rounded-lg text-xl p-2 text-center dark:bg-yellow-400 dark:hover:bg-yellow-300 dark:focus:ring-yellow-300"onClick={handleListing}
           >
             Start Listening
           </button>
           <button
-            type="submit"
             className="text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium m-2 rounded-lg text-xl p-2 text-center dark:bg-yellow-400 dark:hover:bg-yellow-300 dark:focus:ring-yellow-300"onClick={stopHandle}
           >
             Stop Listening
+          </button>
+          <button
+            className="text-black bg-yellow-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium m-2 rounded-lg text-xl p-2 text-center dark:bg-yellow-400 dark:hover:bg-yellow-300 dark:focus:ring-yellow-300"onClick={reset}
+          >
+          Reset
           </button>
           </div>
 
